04ebf8e370825afc0899d7766593f19b948d3978
ehasanulh1 <ehasanulh1@gmail.com>
2022-12-01 22:12:12 +0600

    today is finish

--- src/components/Shipping/Shipping.js
+++ src/components/Shipping/Shipping.js
@@ -5,6 +5,8 @@ const Shipping = () => {
     return (
         <div>
             <h2>Shipping</h2>
+            <p>Your shipping is on the way</p>
+            <p><small>Comming Soon!!!</small></p>
         </div>
     );
 };
