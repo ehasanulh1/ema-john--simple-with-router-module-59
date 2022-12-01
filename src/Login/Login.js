12c032dabbb5a2a62616d295a79e2c45d90cd3cb
ehasanulh1 <ehasanulh1@gmail.com>
2022-12-01 20:32:07 +0600

    UserContex

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -1,10 +1,42 @@
 import React from 'react';
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../contexts/UserContext';
 import './Login.css';
 
 const Login = () => {
+    const { signIn } = useContext(AuthContext);
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+                form.reset();
+            })
+            .catch(error => console.error(error))
+    }
+
     return (
-        <div>
-            <h2>this is Log in</h2>
+        <div className='form-container'>
+            <h2 className='form-title'>Login</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-control">
+                    <label htmlFor="email">Email</label>
+                    <input type="email" name='email' required />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name='password' required />
+                </div>
+                <input className='btn-submit' type="submit" value="Login" />
+            </form>
+            <p>New to ema-john <Link to='/signup'>Create a New Account</Link></p>
         </div>
     );
 };
--- src/_auth.steps.js
+++ src/_auth.steps.js
@@ -7,3 +7,14 @@ Auth Setup
 5. get firebase config in your firebase.config.js file
 6. export app from firebase.config.js
 */
+
+/*
+1. Create UserCOntext (Auth Context); UserContext --> component name userContext provides AuthContext
+
+2. create Auth context
+3. set AuthContext.Provider
+4. Make sure you set the {children}
+5. export AuthContext to be used inside useContext hook
+6. get from data
+7. getAuth in the UserContext
+*/
